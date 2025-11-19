9cc4d401e6fa0ec1ecfc3edc26f68287e5355de8
Jyoti Mishra <JyotiM1@verifone.com>
2025-11-19 09:02:41 +0530

    deleted old files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="src",
-    version="0.0.1",
-    author="Jyoti Ranjan Mishra",
-    author_email="jyotiranjan.mishra2020@gmail.com",
-    packages=find_packages()
-)
